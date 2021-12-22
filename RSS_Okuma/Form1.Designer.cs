db61b90f276c4aba8e19d87836d1ed332321f89b
salihkoc <salihkoc-e@users.noreply.github.com>
2021-12-22 20:30:53 +0300

    Add project files.

---
+++ RSS_Okuma/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace RSS_Okuma
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGetir = new System.Windows.Forms.Button();
+            this.lstBaslik = new System.Windows.Forms.ListBox();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.txtRss = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnGetir
+            // 
+            this.btnGetir.Location = new System.Drawing.Point(973, 27);
+            this.btnGetir.Name = "btnGetir";
+            this.btnGetir.Size = new System.Drawing.Size(113, 27);
+            this.btnGetir.TabIndex = 0;
+            this.btnGetir.Text = "Kaynağı Getir";
+            this.btnGetir.UseVisualStyleBackColor = true;
+            this.btnGetir.Click += new System.EventHandler(this.btnGetir_Click);
+            // 
+            // lstBaslik
+            // 
+            this.lstBaslik.FormattingEnabled = true;
+            this.lstBaslik.ItemHeight = 16;
+            this.lstBaslik.Location = new System.Drawing.Point(22, 76);
+            this.lstBaslik.Name = "lstBaslik";
+            this.lstBaslik.Size = new System.Drawing.Size(297, 548);
+            this.lstBaslik.TabIndex = 1;
+            this.lstBaslik.SelectedIndexChanged += new System.EventHandler(this.lstBaslik_SelectedIndexChanged);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(343, 76);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(743, 548);
+            this.webBrowser1.TabIndex = 2;
+            // 
+            // txtRss
+            // 
+            this.txtRss.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtRss.Location = new System.Drawing.Point(22, 26);
+            this.txtRss.Name = "txtRss";
+            this.txtRss.Size = new System.Drawing.Size(932, 27);
+            this.txtRss.TabIndex = 3;
+            this.txtRss.Text = "https://www.sabah.com.tr/rss/spor.xml";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 8.25F);
+            this.label1.Location = new System.Drawing.Point(19, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Haber Başlığı";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 8.25F);
+            this.label2.Location = new System.Drawing.Point(19, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Haber Başlığı";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1131, 644);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstBaslik);
+            this.Controls.Add(this.txtRss);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.btnGetir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGetir;
+        private System.Windows.Forms.ListBox lstBaslik;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox txtRss;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RSS_Okuma/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace RSS_Okuma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGetir_Click(object sender, EventArgs e)
+        {
+
+            List<Haber> kayitlar = xmlCevir();
+            lstBaslik.DataSource = kayitlar;
+
+        }
+
+
+        private List<Haber> xmlCevir()
+        {
+            List<Haber> haberKayitlari = new List<Haber>();
+            XDocument xmlKaynak = XDocument.Load(txtRss.Text);
+
+            List<XElement> rows = xmlKaynak.Descendants("item").ToList();
+            foreach (XElement item in rows)
+            {
+
+                Haber temp = new Haber();
+                temp.baslik = item.Element("title").Value;
+                temp.link = item.Element("link").Value;
+                temp.aciklama = item.Element("description").Value;
+                haberKayitlari.Add(temp);
+                
+            }
+            return haberKayitlari;
+        }
+
+        private void lstBaslik_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ListBox secilenDeger = (ListBox)sender;
+            Haber secilenHaber = (Haber)secilenDeger.SelectedItem;
+            webBrowser1.DocumentText = secilenHaber.aciklama;
+        }
+    }
+}
